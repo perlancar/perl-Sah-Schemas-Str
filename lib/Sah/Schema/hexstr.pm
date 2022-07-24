811b21abd21c1a233dca9624e0643fb58f5896ab
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-07-24 20:33:14 +0700

    [ux] [schema hexstr] Allow & remove whitespaces

--- lib/Sah/Schema/hexstr.pm
+++ lib/Sah/Schema/hexstr.pm
@@ -11,11 +11,15 @@ our $schema = [str => {
     summary => 'String of bytes in hexadecimal',
     match => qr/\A(?:[0-9A-Fa-f]{2})*\z/,
 
+    prefilters => ['Str::remove_whitespace'],
+
     examples => [
         {value=>'', valid=>1},
         {value=>'a0', valid=>1},
+        {value=>'A0', valid=>1, summary=>'Uppercase digits are allowed, not coerced to lowercase'},
         {value=>'a0f', valid=>0, summary=>'Odd number of digits (3)'},
         {value=>'a0ff', valid=>1},
+        {value=>'a0 ff 61', valid=>1, summary=>'Whitespace will be removed', validated_value=>'a0ff61'},
         {value=>'a0fg', valid=>0, summary=>'Invalid hexdigit (g)'},
         {value=>'a0ff12345678', valid=>1},
     ],
